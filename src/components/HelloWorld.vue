737f5cc8b06f4e06ef3bfa66d15a14372a9a3fbb
Vinicius Cainelli <vicainelli+github@gmail.com>
2020-07-13 17:23:44 -0300

    Adding Tailwind config and replacing css to classes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,45 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div>
+    <h1 class="text-3xl font-bold text-gray-800 my-12">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+      <a class="text-green-500" href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3 class="text-xl font-semibold text-gray-700 mt-8 mb-2">Installed CLI Plugins</h3>
     <ul>
-      <li>
+      <li class="inline-block mx-4">
         <a
+          class="text-green-500"
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
           >babel</a
         >
       </li>
-      <li>
+      <li class="inline-block mx-4">
         <a
+          class="text-green-500"
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
           >eslint</a
         >
       </li>
-      <li>
+      <li class="inline-block mx-4">
         <a
+          class="text-green-500"
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
           target="_blank"
           rel="noopener"
           >unit-jest</a
         >
       </li>
-      <li>
+      <li class="inline-block mx-4">
         <a
+          class="text-green-500"
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress"
           target="_blank"
           rel="noopener"
@@ -43,55 +47,63 @@
         >
       </li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3 class="text-xl font-semibold text-gray-700 mt-8 mb-2">Essential Links</h3>
     <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      <li class="inline-block mx-4">
+        <a class="text-green-500" href="https://vuejs.org" target="_blank" rel="noopener"
+          >Core Docs</a
+        >
       </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+      <li class="inline-block mx-4">
+        <a class="text-green-500" href="https://forum.vuejs.org" target="_blank" rel="noopener"
           >Forum</a
         >
       </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+      <li class="inline-block mx-4">
+        <a class="text-green-500" href="https://chat.vuejs.org" target="_blank" rel="noopener"
           >Community Chat</a
         >
       </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+      <li class="inline-block mx-4">
+        <a class="text-green-500" href="https://twitter.com/vuejs" target="_blank" rel="noopener"
           >Twitter</a
         >
       </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      <li class="inline-block mx-4">
+        <a class="text-green-500" href="https://news.vuejs.org" target="_blank" rel="noopener"
+          >News</a
+        >
       </li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3 class="text-xl font-semibold text-gray-700 mt-8 mb-2">Ecosystem</h3>
     <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+      <li class="inline-block mx-4">
+        <a class="text-green-500" href="https://router.vuejs.org" target="_blank" rel="noopener"
           >vue-router</a
         >
       </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      <li class="inline-block mx-4">
+        <a class="text-green-500" href="https://vuex.vuejs.org" target="_blank" rel="noopener"
+          >vuex</a
+        >
       </li>
-      <li>
+      <li class="inline-block mx-4">
         <a
+          class="text-green-500"
           href="https://github.com/vuejs/vue-devtools#vue-devtools"
           target="_blank"
           rel="noopener"
           >vue-devtools</a
         >
       </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+      <li class="inline-block mx-4">
+        <a class="text-green-500" href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
           >vue-loader</a
         >
       </li>
-      <li>
+      <li class="inline-block mx-4">
         <a
+          class="text-green-500"
           href="https://github.com/vuejs/awesome-vue"
           target="_blank"
           rel="noopener"
@@ -104,27 +116,9 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String
-  }
-};
+    msg: String,
+  },
+}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
